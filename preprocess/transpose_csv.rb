cc8a265b6cb4bd2d0b8c23c1318ac352accf6ff6
Matti Ahinko <matti.ahinko@gmail.com>
2016-05-15 21:07:48 +0300

    Add setting separator

--- preprocess/transpose_csv.rb
+++ preprocess/transpose_csv.rb
@@ -6,15 +6,19 @@ def help
   puts "ruby transpose_csv.rb [INPUT_FILE] [SEPARATOR, (optional)]"
 end
 
-abort(help) if (ARGV[0].nil?
+abort(help) if ARGV[0].nil?
 
 csv_matrix = []
 
-CSV.foreach(ARGV[0]) do |row|
+options = {}
+
+options[:col_sep] = ARGV[1] unless ARGV[1].nil?
+
+CSV.foreach(ARGV[0], options) do |row|
   csv_matrix << row
 end
 
-CSV.open("transpose_#{ARGV[0]}", "w") do |csv|
+CSV.open("transpose_#{ARGV[0]}", "w", options) do |csv|
   csv_matrix.transpose.each do |row|
     csv << row
   end
