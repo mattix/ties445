e242f314f9f2b6767d926ce5c0ec72c627df7d20
Matti Ahinko <matti.ahinko@gmail.com>
2016-05-15 20:58:46 +0300

    Add script for transposing csv

---
+++ preprocess/transpose_csv.rb
@@ -0,0 +1,14 @@
+#!/bin/ruby
+abort("No filename given or file does not exist") if (ARGV[0].nil? || !File.exist(ARGV[0]))
+
+csv_matrix = []
+
+CSV.foreach(ARGV[0]) do |row|
+  csv_matrix << row
+end
+
+CSV.open("transpose_#{ARGV[0]}", "w") do |csv|
+  csv_matrix.transpose.each do |row|
+    csv << row
+  end
+end
